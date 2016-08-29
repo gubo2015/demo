18c572fddf83766f09e7f8fdc16954e56dbf4317
gubo2015 <356112797@qq.com>
2016-08-29 18:59:38 +0800

    chayige

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,10 @@ class WelcomeController < ApplicationController
   def index
     @users = User.all
     render json:@users
+
+  end
+  def show
+    @user = User.find(params[:id])
+    render json:@user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # get 'welcome/index'
   post 'welcome/index'
+  get 'welcome/:id'=>'welcome#show'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
