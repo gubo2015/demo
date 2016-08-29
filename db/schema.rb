e50cf73bbe5b22d152ea1d6ece6a1b3b67ade10f
gubo2015 <356112797@qq.com>
2016-08-29 17:56:43 +0800

    add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826095535) do
+ActiveRecord::Schema.define(version: 20160829094056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "addresses", force: :cascade do |t|
-    t.string   "address"
-    t.string   "mobile"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password"
-    t.string   "address"
+    t.string   "mobile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "addresses", "users"
 end
